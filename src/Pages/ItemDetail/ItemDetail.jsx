081f9a34da6a3f64934958d88d6d3b7f40e0892a
Declan Brown <135834907+DeclanKBrown@users.noreply.github.com>
2023-09-22 20:02:34 +1000

    Added item detail page

--- src/Pages/ItemDetail/ItemDetail.jsx
+++ src/Pages/ItemDetail/ItemDetail.jsx
@@ -1,15 +1,33 @@
+import { useState } from 'react';
 import { items } from '../../Utilities/items'
 import { useLoaderData } from "react-router-dom";
 
 export function loader({ params }) {
     const item = items.find(item => item.name === params.name)
+    console.log(item)
     return { item }
 }
 
+
 export default function ItemDetail() {
-    const item = useLoaderData()
+    const [qty, setQty] = useState(1)
+
+    const { item } = useLoaderData()
     return (
-        <>
-        </>
+        <main className='min-h-[89vh] py-16'>
+            <div className="grid grid-cols-2">
+                <div className='grid grid-cols-2 gap-3'>
+                    <img src={item.imageFront} alt='imageFront' />
+                    <img src={item.imageBack} alt='imageBack'/>
+                </div>
+                <div className='flex flex-col px-16 pr-80'>
+                    <h1 className='text-3xl font-bold mb-6'>{item.name}</h1>
+                    <h2 className='mb-4'>${item.price} AUD</h2>
+                    <input type="number" className='border border-black mb-8 pl-4 w-20' min='1' max='5' value={qty} onChange={(e) => {setQty(e.value)}}/>
+                    <button className='p-4 border border-black mb-6'>Add To Cart</button>
+                    <span className='mb-4'>{item.description}</span>
+                </div>
+            </div>
+        </main>
     )
 }
--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -3,7 +3,7 @@ import ItemCard from "../../Components/ItemCard/ItemCard"
 
 export default function Shop() {
     return (
-        <main className="min-h-[89vh] flex flex-col sm:px-16 md:px-32 lg:px-64 xl:px-64 py-4">
+        <main className="min-h-[89vh] flex flex-col sm:px-16 md:px-32 lg:px-64 xl:px-64 py-4 items-center">
             <div className="grid grid-cols-3 gap-4">
                 {items.map(item => (
                     <ItemCard 
