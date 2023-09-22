5938e3db3f0fc93988c743c6da45d72f2b2670f5
Declan Brown <135834907+DeclanKBrown@users.noreply.github.com>
2023-09-22 22:55:36 +1000

    Added addtoCart function

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,12 +1,24 @@
 import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
 import { Outlet } from "react-router-dom";
+import { useState } from 'react';
+
 
 export default function Root() {
+    const [cart, setCart] = useState([])
+    
+    function addToCart(item) {
+        setCart([...cart, item])
+    }
+
     return (
         <>
-            <Header />
-            <Outlet />
+            <Header 
+                numItems={cart.length}
+            />
+            <Outlet 
+                addToCart={addToCart}
+            />
             <Footer />
         </>
         )
